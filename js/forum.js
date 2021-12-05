0bf7b88b5ccdaebe439a5ac072b6dc6532f0958e
Nicholas Weidman <nweidman@stevens.edu>
2021-12-05 16:13:53 -0500

    Implemented an add and remove post function for the forum page

---
+++ js/forum.js
@@ -0,0 +1,31 @@
+var comment0 = document.getElementById("0");
+var comment1 = document.getElementById("1");
+var comment2 = document.getElementById("2");
+var submitButton = document.getElementById("submit");
+comment0.onclick=function(){deleteText(comment0)};
+comment1.onclick=function(){deleteText(comment1)};
+comment2.onclick=function(){deleteText(comment2)};
+submitButton.onclick = function(){submitText()};
+
+
+
+function submitText(){
+    var text = document.getElementById("forumText");
+    var postList = document.getElementById("posts");
+    if(text.value.length>=1){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(text.value));
+        var newButton = document.createElement("button");
+        newButton.innerHTML = "Delete";
+        newButton.id = (parseInt(postList.lastElementChild.lastElementChild.id) + 1) + ""
+        newButton.onclick = function(){deleteText(newButton)};
+        li.appendChild(newButton);
+        postList.appendChild(li);
+        text.value="";
+    }
+}
+
+function deleteText(button){
+        var li = button.parentElement;
+        li.remove();
+}
