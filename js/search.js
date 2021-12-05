75e424fd110c29fc6480cdcf3acf4f19d7176bd2
Jason Qiu <jqiu21@protonmail.com>
2021-12-05 11:22:38 -0500

    Added different functions for searching all movies and all genres

--- js/search.js
+++ js/search.js
@@ -1,6 +1,25 @@
-function search(){
+function searchMovies(){
     var input, filtered, movies, i, a;
-    input = document.getElementById("search");
+    input = document.getElementById("searchMovies");
+    filtered = input.value.replace(/[^A-Za-z0-9]/g, "").toUpperCase();
+    movies = document.getElementsByClassName("dropitm");
+
+    for(i = 1; i<movies.length; i++){
+        a = movies[i].innerHTML.replace(/[^A-Za-z0-9]/g,"").toUpperCase();
+        console.log(a)
+        if (a.indexOf(filtered)>-1){
+            console.log(a.indexOf(filtered)>-1);
+            movies[i].style.display="block";
+        } else {
+            movies[i].style.display="none";
+        }
+
+    }
+}
+
+function searchGenres(){
+    var input, filtered, movies, i, a;
+    input = document.getElementById("searchGenres");
     filtered = input.value.replace(/[^A-Za-z0-9]/g, "").toUpperCase();
     movies = document.getElementsByClassName("dropitm");
 
