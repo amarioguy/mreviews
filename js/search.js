65b835533227194d43da7627de0db4f3a82716d7
Jason Qiu <jqiu21@protonmail.com>
2021-12-05 11:45:53 -0500

    Added Genre Menu

--- js/search.js
+++ js/search.js
@@ -2,13 +2,11 @@ function searchMovies(){
     var input, filtered, movies, i, a;
     input = document.getElementById("searchMovies");
     filtered = input.value.replace(/[^A-Za-z0-9]/g, "").toUpperCase();
-    movies = document.getElementsByClassName("dropitm");
+    movies = document.getElementById("movie-menu").getElementsByClassName("dropitm");
 
     for(i = 1; i<movies.length; i++){
         a = movies[i].innerHTML.replace(/[^A-Za-z0-9]/g,"").toUpperCase();
-        console.log(a)
         if (a.indexOf(filtered)>-1){
-            console.log(a.indexOf(filtered)>-1);
             movies[i].style.display="block";
         } else {
             movies[i].style.display="none";
@@ -18,19 +16,17 @@ function searchMovies(){
 }
 
 function searchGenres(){
-    var input, filtered, movies, i, a;
+    var input, filtered, genres, i, a;
     input = document.getElementById("searchGenres");
     filtered = input.value.replace(/[^A-Za-z0-9]/g, "").toUpperCase();
-    movies = document.getElementsByClassName("dropitm");
+    genres = document.getElementById("genre-menu").getElementsByClassName("dropitm");
 
-    for(i = 1; i<movies.length; i++){
-        a = movies[i].innerHTML.replace(/[^A-Za-z0-9]/g,"").toUpperCase();
-        console.log(a)
+    for(i = 1; i<genres.length; i++){
+        a = genres[i].innerHTML.replace(/[^A-Za-z0-9]/g,"").toUpperCase();
         if (a.indexOf(filtered)>-1){
-            console.log(a.indexOf(filtered)>-1);
-            movies[i].style.display="block";
+            genres[i].style.display="block";
         } else {
-            movies[i].style.display="none";
+            genres[i].style.display="none";
         }
 
     }
